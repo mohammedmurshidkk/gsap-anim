28fe94fe3c6142f7f5d61f7117056955274b3fcf
mohammedmurshidkk <muhammedmurshid43@gmail.com>
2024-05-04 12:51:33 +0530

    Config

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: false,
+  output: 'export',
+  basePath: 'gsap-anim',
+  reactStrictMode: true,
   images: { domains: ['picsum.photos'] },
 };
 
